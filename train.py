f0de87387dea39f00e25d318a08aa29ea8e6049a
Andre Sprenger <andre.sprenger@xing.com>
2018-07-16 23:10:19 +0200

    Add more options to train.py

--- train.py
+++ train.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
-"""
-Train a Doc2vec model.
-"""
+"""Train a Doc2vec model from a collection of documents."""
 
 import os
+import sys
 import codecs
 import argparse
 import logging
@@ -26,8 +25,9 @@ def read_lines(path):
 def current_time_ms():
     return int(time.time()*1000.0)
 
-def make_timestamped_dir(base_path):
-    output_path = os.path.join(base_path, str(current_time_ms()) + '_' + str(randint(1000000, 9999999)))
+def make_timestamped_dir(base_path, algorithm, vector_size, epochs, window):
+    suffix = '%s_dim=%d_window=%d_epochs=%d' % (algorithm, vector_size, window, epochs)
+    output_path = os.path.join(base_path, str(current_time_ms())) + '_' + suffix
     clean_make_dir(output_path)
     return output_path
 
@@ -43,7 +43,7 @@ def load_stopwords(stopwords_path):
 TaggedDocument = namedtuple('TaggedDocument', 'tags words')
 
 
-def run(doc_path, output_base_dir, stopwords_path, vocab_min_count, num_epochs, algorithm, vector_size, alpha, min_alpha, train):
+def run(doc_path, output_base_dir, stopwords_path, vocab_min_count, num_epochs, algorithm, vector_size, alpha, min_alpha, train, window, cores):
     
     # As soon as FAST_VERSION is not -1, there are compute-intensive codepaths that avoid holding 
     # the python global interpreter lock, and thus you should start to see multiple cores engaged.
@@ -52,7 +52,8 @@ def run(doc_path, output_base_dir, stopwords_path, vocab_min_count, num_epochs, 
 
     stopwords = load_stopwords(stopwords_path)
 
-    cores = multiprocessing.cpu_count()
+    if cores == None:
+        cores = multiprocessing.cpu_count()
 
     all_docs = []
 
@@ -73,24 +74,31 @@ def run(doc_path, output_base_dir, stopwords_path, vocab_min_count, num_epochs, 
         if doc_id % 10000 == 0:
             logging.info('Loaded %s documents', doc_id)
 
+
+    negative = 5
+    hs = 0
+
+
     if algorithm == 'pv_dmc':
         # PV-DM with concatenation
         # window=5 (both sides) approximates paper's 10-word total window size
         # PV-DM w/ concatenation adds a special null token to the vocabulary: '\x00'
-        model = Doc2Vec(dm=1, dm_concat=1, vector_size=vector_size, window=5, negative=5, hs=0, 
+        model = Doc2Vec(dm=1, dm_concat=1, vector_size=vector_size, window=window, negative=negative, hs=hs, 
                         min_count=vocab_min_count, workers=cores)
     elif algorithm == 'pv_dma':
         # PV-DM with average
         # window=5 (both sides) approximates paper's 10-word total window size
-        model = Doc2Vec(dm=1, dm_mean=1, vector_size=vector_size, window=5, negative=5, hs=0, 
+        model = Doc2Vec(dm=1, dm_mean=1, vector_size=vector_size, window=window, negative=negative, hs=hs, 
                         min_count=vocab_min_count, workers=cores)
     elif algorithm == 'pv_dbow':
         # PV-DBOW 
-        model = Doc2Vec(dm=0, vector_size=vector_size, negative=5, hs=0, min_count=vocab_min_count, 
-                        workers=cores)
+        model = Doc2Vec(dm=0, vector_size=vector_size, window=window, negative=negative, hs=hs, 
+                        min_count=vocab_min_count, workers=cores)
     else:
         raise ValueError('Unknown algorithm: %s' % algorithm)
         
+
+
     logging.info('Algorithm: %s' % str(model))
 
     logging.info('Build vocabulary')
@@ -98,8 +106,7 @@ def run(doc_path, output_base_dir, stopwords_path, vocab_min_count, num_epochs, 
     vocab_size = len(model.wv.vocab)
     logging.info('Vocabulary size: %d', vocab_size)
 
-    target_dir = make_timestamped_dir(output_base_dir)
-
+    target_dir = make_timestamped_dir(output_base_dir, algorithm, model.vector_size, num_epochs, window)
     vocab_path = os.path.join(target_dir, 'vocabulary')
     logging.info('Save vocabulary to: %s', vocab_path)
     with open(vocab_path, 'w') as f:
@@ -110,31 +117,32 @@ def run(doc_path, output_base_dir, stopwords_path, vocab_min_count, num_epochs, 
 
     if train:        
 
-        alpha_delta = (alpha - min_alpha) / num_epochs
-
         logging.info('Shuffle documents')
         shuffle(all_docs)
 
-        logging.info('Start training')
+        logging.info('Train model')
         model.train(all_docs, total_examples=len(all_docs), epochs=num_epochs, start_alpha=alpha, end_alpha=min_alpha)
-        
-        model.delete_temporary_training_data(keep_doctags_vectors=True, keep_inference=True)
 
-        logging.info('Save model to: %s', target_dir)
+        logging.info('Save model to: %s', target_dir)        
+        model.delete_temporary_training_data(keep_doctags_vectors=True, keep_inference=True)
         model.save(os.path.join(target_dir, 'doc2vec.model'))
 
         model_meta = {
+            'argv': sys.argv,
             'doc_path': doc_path,
-            'target_dir': target_dir,
             'stopwords_path': stopwords_path,
+            'target_dir': target_dir,
             'algorithm': algorithm,
-            'vocab_min_count': vocab_min_count,
-            'num_epochs': num_epochs,
-            'algorithm': algorithm,
+            'window': window,
             'vector_size': vector_size,
             'alpha': alpha,
             'min_alpha': min_alpha,
-            'vocab_size': vocab_size
+            'num_epochs': num_epochs,
+            'vocab_min_count': vocab_min_count,
+            'vocab_size': vocab_size,
+            'cores': cores,
+            'negative': negative,
+            'hs': hs
         }
 
         model_meta_path = os.path.join(target_dir, 'model.meta')
@@ -142,14 +150,17 @@ def run(doc_path, output_base_dir, stopwords_path, vocab_min_count, num_epochs, 
         with open(model_meta_path, 'w') as outfile:
             json.dump(model_meta, outfile)
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--algorithm', help='train algorithm, one of: [pv_dmc, pv_dma, pv_dbow]', default='pv_dmc')
     parser.add_argument('--vocab-min-count', help='ignores all words with total frequency lower than this.', type=int, default=10)
-    parser.add_argument('--vector-size', help='word and document vector size', type=int, default=100)
+    parser.add_argument('--dim', help='word and document vector size', type=int, default=100)
+    parser.add_argument('--window', help='window size', type=int, default=5)
     parser.add_argument('--epochs', help='number of training epochs', type=int, default=20)
     parser.add_argument('--alpha', help='initial learning rate', type=float, default=0.025)
     parser.add_argument('--min-alpha', help='learning rate will linearly drop to min_alpha as training progresses', type=float, default=0.001)
+    parser.add_argument('--cores', help='number of cores to train on', type=int)
     parser.add_argument('--train', help='train model', dest='train', action='store_true')
     parser.add_argument('--no-train', help='no model training, only generate vocabulary', dest='train', action='store_false')
     parser.set_defaults(train=True)
@@ -159,8 +170,8 @@ def main():
     args = parser.parse_args()
     logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s', level=logging.INFO)
     run(doc_path=args.input_path, output_base_dir=args.output_base_dir, stopwords_path=args.stopwords, vocab_min_count=args.vocab_min_count, 
-        num_epochs=args.epochs, algorithm=args.algorithm, vector_size=args.vector_size, alpha=args.alpha, min_alpha=args.min_alpha, 
-        train=args.train)
+        num_epochs=args.epochs, algorithm=args.algorithm, vector_size=args.dim, alpha=args.alpha, min_alpha=args.min_alpha, 
+        train=args.train, window=args.window, cores=args.cores)
 
 if __name__ == '__main__':
     main()
